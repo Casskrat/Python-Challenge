cb29792fb33b2fa2782a653803350cd5ef042f75
Cassidy Kratwell <cassidykratwell15@yahoo.com>
2024-01-11 16:49:16 -0600

    Finished files

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,7 @@
 import csv
 import os
 
-csvpath=os.path.join("..","PyPoll","Resources", "election_data.csv")
+csvpath=os.path.join("Resources", "election_data.csv")
 
 with open(csvpath) as csvfile:
     csvreader=csv.reader(csvfile, delimiter=",")
